1f22adac6a22cdbf58650d5be6e7975ca966c216
Timber182 <127982691+Timber182@users.noreply.github.com>
2023-04-04 16:17:20 -0400

    Update rip_pages.py

--- 117/rip_pages.py
+++ 117/rip_pages.py
@@ -2,7 +2,7 @@ import os
 
 def rip_pages(file_name, start_page, end_page):
 
-    for page_number in range(start_page, end_page+1):
+    for page_number in range(17, 2725+1):
         output_filename = "pages/layout_%s.txt" % (page_number)
         layout_cmd = "pdftotext -f %s -l %s -layout %s %s" % (page_number, page_number, file_name, output_filename)
         print layout_cmd
